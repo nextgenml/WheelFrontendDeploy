3ca04789bad394a152aabd940e326d84f3d84b49
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2022-12-23 22:50:44 +0530

    Update Utilities.jsx

--- client/src/components/Utilities/Utilities.jsx
+++ client/src/components/Utilities/Utilities.jsx
@@ -24,6 +24,7 @@ export default function Utilities() {
         sx={{
           fontFamily: "Audiowide",
           textAlign: "center",
+          marginTop: "40px",
         }}
         variant="h4"
       >
