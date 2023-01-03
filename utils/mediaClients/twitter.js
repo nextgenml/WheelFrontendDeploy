c090358ec7d9ab92d1480895d5ca764aa3fc54a8
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-01-03 21:08:46 +0530

    Update twitter.js

--- utils/mediaClients/twitter.js
+++ utils/mediaClients/twitter.js
@@ -6,9 +6,10 @@ const readOnlyClient = twitterClient.readOnly;
 
 const searchTweets = async (search, start_time, end_time) => {
   const jsTweets = await readOnlyClient.v2.search(`"${search}" -is:retweet`, {
-    "tweet.fields": ["conversation_id", "created_at"],
-    expansions: "author_id",
+    "tweet.fields": ["conversation_id", "created_at", "attachments"],
+    expansions: ["author_id", "attachments.media_keys"],
     "user.fields": ["name"],
+    "media.fields": ["url"],
     max_results: 100,
     start_time,
     end_time,
@@ -18,16 +19,19 @@ const searchTweets = async (search, start_time, end_time) => {
 
   const result = [];
   for await (const tweet of jsTweets) {
-    const data = includes.author(tweet);
-    // console.log(tweet, data);
+    const author = includes.author(tweet);
+    const medias = includes.medias(tweet);
+    // console.log(tweet, author, medias);
     result.push({
       postId: tweet.id,
-      username: data.username,
-      postLink: `https://twitter.com/${data.username}/status/${tweet.id}`,
+      username: author.username,
+      postLink: `https://twitter.com/${author.username}/status/${tweet.id}`,
       createdAt: tweet.created_at,
+      imageUrls: medias.filter((x) => x.type == "photo").map((x) => x.url),
     });
   }
 
+  console.log("result", result);
   return result;
 };
 
@@ -62,8 +66,8 @@ const tweetReplies = async () => {
   const jsTweets = await readOnlyClient.v2.search(
     "conversation_id:1607679115536072704",
     {
-      "tweet.fields": ["conversation_id"],
-      expansions: "author_id",
+      "tweet.fields": ["conversation_id", "attachments"],
+      expansions: ["author_id", "attachments"],
       "user.fields": ["location", "name"],
       max_results: 100,
     }
@@ -76,7 +80,9 @@ const tweetReplies = async () => {
     console.log(tweet, data);
   }
 };
-// searchTweets("javascript");
+// searchTweets(
+//   "One of the goals for me this year is to travel to 5 countries.And I know with God, it is popsible."
+// );
 // tweetLikedUsers();
 // retweetedUsers();
 // tweetReplies();
