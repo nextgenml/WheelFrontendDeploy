4b3f8708c71482a6fee4ec95589d6e9347294547
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-01-08 19:48:24 +0530

    Update 20230108133503-columns_chores_1.js

--- migrations/20230108133503-columns_chores_1.js
+++ migrations/20230108133503-columns_chores_1.js
@@ -8,9 +8,16 @@ module.exports = {
       "twitter_id",
       Sequelize.STRING(255)
     );
+
+    await queryInterface.addColumn(
+      "campaigns",
+      "wallet_id",
+      Sequelize.STRING(255)
+    );
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn("holders", "twitter_id");
+    await queryInterface.removeColumn("campaigns", "wallet_id");
   },
 };
