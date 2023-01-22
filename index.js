1aaebcfaa60763d1ebdfd30c1d4cce70d182ecf9
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-01-22 23:05:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const moment = require("moment");
 require("./manager/spinwheel");
 require("./manager/wallet");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
 const { static } = require("express");
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
+  }
+})
+var upload = multer({ storage: storage });
 
 const {
   getParticipants,
