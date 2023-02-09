31e17150b63228b114cd123ac08f4e4cbc7cc420
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-02-10 00:31:01 +0530

    done

--- client/src/components/SocialSharing/Instructions.jsx
+++ client/src/components/SocialSharing/Instructions.jsx
@@ -13,9 +13,10 @@ import {
   Grid,
   Typography,
   Box,
+  Link,
 } from "@mui/material";
 import config from "../../config";
-
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 const Instructions = ({ generatedAlias, address }) => {
   const [open, setOpen] = React.useState(false);
   const [alias, setAlias] = React.useState(generatedAlias || "");
@@ -73,13 +74,108 @@ const Instructions = ({ generatedAlias, address }) => {
         <DialogContent>
           <List>
             <ListItem>
-              Let Google help apps determine location. This means sending
-              anonymous location data to Google, even when no apps are running.
+              <Typography variant="h6">MetaMask setup.</Typography>
             </ListItem>
             <ListItem>
-              Let Google help apps determine location. This means sending
-              anonymous location data to Google, even when no apps are running.
+              <Typography variant="body2">
+                1. Download MetaMask on your phone or Laptop. -&nbsp;&nbsp;
+                <Link
+                  href={"https://youtu.be/-HTubEJ61zU"}
+                  target="_blank"
+                  underline="none"
+                >
+                  Video &nbsp;
+                  <OpenInNewIcon fontSize="small" />
+                </Link>
+              </Typography>
             </ListItem>
+            <ListItem>
+              <Typography variant="body2">
+                2. Please ensure you have a brand new wallet as this will be
+                used as your login
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="body2">
+                3. Request funny ETH from a faucet for testing- steps
+                &nbsp;&nbsp;
+              </Typography>
+              <Link
+                // add faucet testing pdf
+                href={"/faucet_testing.pdf"}
+                target="_blank"
+                underline="none"
+              >
+                Doc &nbsp;
+                <OpenInNewIcon fontSize="small" />
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Typography variant="body2">
+                4. Click on Connect button on the web
+              </Typography>
+            </ListItem>
+          </List>
+
+          <List>
+            <ListItem>
+              <Typography variant="h6">Twitter setup.</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="body2">
+                1. create a Twitter account
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="body2">
+                2. Update your Twitter @ handle to what is mentioned in web page
+              </Typography>
+            </ListItem>
+          </List>
+          <List>
+            <ListItem>
+              <Typography variant="h6">Click on assigned chores</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="body2">
+                1. You may be provided about 50 - 100 chores
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="body2">
+                2. Every chore is a combination of clicks
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="body2">
+                3. Complete the assigned chores
+              </Typography>
+            </ListItem>
+          </List>
+          <List>
+            <ListItem>
+              <Typography variant="h6">Review</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="body2">
+                1. Please let us know if the user interface was easy to use
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="body2">
+                2. Will you use the tool for SEO campaigns or event promotions
+              </Typography>
+            </ListItem>
+          </List>
+          <List>
+            <ListItem>
+              <Typography variant="h6">Timeline</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="body2">13th Feb to 20th Feb</Typography>
+            </ListItem>
+          </List>
+          <List>
             <ListItem sx={{ mt: 3 }}>
               <Grid container spacing={2}>
                 <Grid item md={2}>
--- dbconnect.js
+++ dbconnect.js
@@ -64,5 +64,6 @@ module.exports = {
 // brew install mysql
 // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 // flush privileges;
+// GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION
 // npx sequelize-cli migration:create --name create_spin_tables
 //  npx sequelize-cli db:migrate
