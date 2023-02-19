39ba0ea0b28ab22d5964b0c144a71b32c826212b
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-02-19 13:43:55 +0530

    Update twitter.js

--- utils/mediaClients/twitter.js
+++ utils/mediaClients/twitter.js
@@ -1,102 +1,143 @@
 const { TwitterApi, TwitterV2IncludesHelper } = require("twitter-api-v2");
 const config = require("../../config.js");
+const logger = require("../../logger.js");
+const { timer } = require("../index.js");
 
 let twitterClient = new TwitterApi(config.TWITTER_DEV_TOKEN);
 const readOnlyClient = twitterClient.readOnly;
 
 const searchTweets = async (search, start_time, end_time) => {
-  console.log("searching in twitter", `"${search}" -is:retweet`);
-  const jsTweets = await readOnlyClient.v2.search(`"${search}" -is:retweet`, {
-    "tweet.fields": ["conversation_id", "created_at", "attachments"],
-    expansions: ["author_id", "attachments.media_keys"],
-    "user.fields": ["name"],
-    "media.fields": ["url"],
-    max_results: 100,
-    start_time,
-    end_time,
-  });
-
-  const includes = new TwitterV2IncludesHelper(jsTweets);
-
-  const result = [];
-  for await (const tweet of jsTweets) {
-    const author = includes.author(tweet);
-    const medias = includes.medias(tweet);
-    // console.log(tweet, author, medias);
-    result.push({
-      postId: tweet.id,
-      username: author.username,
-      postLink: `https://twitter.com/${author.username}/status/${tweet.id}`,
-      createdAt: tweet.created_at,
-      imageUrls: medias.filter((x) => x.type == "photo").map((x) => x.url),
-      followLink: `https://twitter.com/${author.username}`,
-      userId: author.id,
+  try {
+    console.log("searching in twitter", `"${search}" -is:retweet`);
+    const jsTweets = await readOnlyClient.v2.search(`"${search}" -is:retweet`, {
+      "tweet.fields": ["conversation_id", "created_at", "attachments"],
+      expansions: ["author_id", "attachments.media_keys"],
+      "user.fields": ["name"],
+      "media.fields": ["url"],
+      max_results: 100,
+      start_time,
+      end_time,
     });
-  }
 
-  console.log("result", result);
-  return result;
+    const includes = new TwitterV2IncludesHelper(jsTweets);
+
+    const result = [];
+    for await (const tweet of jsTweets) {
+      const author = includes.author(tweet);
+      const medias = includes.medias(tweet);
+      // console.log(tweet, author, medias);
+      result.push({
+        postId: tweet.id,
+        username: author.username,
+        postLink: `https://twitter.com/${author.username}/status/${tweet.id}`,
+        createdAt: tweet.created_at,
+        imageUrls: medias.filter((x) => x.type == "photo").map((x) => x.url),
+        followLink: `https://twitter.com/${author.username}`,
+        userId: author.id,
+      });
+    }
+
+    console.log("result", result);
+    return result;
+  } catch (error) {
+    logger.info(`error due to rate limiting - ${error.data.title}`);
+    if (error.data.status === 429) {
+      logger.info("waiting for rating limiting time");
+      await timer(15 * 60 * 1000);
+      return searchTweets(search, start_time, end_time);
+    }
+  }
 };
 
 const tweetLikedUsers = async (postId, start_time, end_time) => {
-  const users = await readOnlyClient.v2.tweetLikedBy(postId, {
-    asPaginator: true,
-    max_results: 100,
-  });
-
-  const result = [];
-  for await (const user of users) {
-    result.push({
-      username: user.username,
+  try {
+    const users = await readOnlyClient.v2.tweetLikedBy(postId, {
+      asPaginator: true,
+      max_results: 100,
     });
-  }
 
-  return result;
+    const result = [];
+    for await (const user of users) {
+      result.push({
+        username: user.username,
+      });
+    }
+    // console.log("result", result);
+    return result;
+  } catch (error) {
+    logger.info(`error due to rate limiting - ${error.data.title}`);
+    if (error.data.status === 429) {
+      logger.info("waiting for rating limiting time");
+      await timer(15 * 60 * 1000);
+      return tweetLikedUsers(postId, start_time, end_time);
+    }
+  }
 };
 
 const retweetedUsers = async (postId, start_time, end_time) => {
-  const users = await readOnlyClient.v2.tweetRetweetedBy(postId, {
-    "tweet.fields": ["text", "created_at"],
-    asPaginator: true,
-    max_results: 100,
-  });
-
-  const result = [];
-  for await (const user of users) {
-    result.push({
-      username: user.username,
+  try {
+    const users = await readOnlyClient.v2.tweetRetweetedBy(postId, {
+      "tweet.fields": ["text", "created_at"],
+      asPaginator: true,
+      max_results: 100,
     });
+
+    const result = [];
+    for await (const user of users) {
+      result.push({
+        username: user.username,
+      });
+    }
+    // console.log("result", result);
+    return result;
+  } catch (error) {
+    logger.info(`error due to rate limiting - ${error.data.title}`);
+    if (error.data.status === 429) {
+      logger.info("waiting for rating limiting time");
+      await timer(15 * 60 * 1000);
+      return retweetedUsers(postId, start_time, end_time);
+    }
   }
-  // console.log("result", result);
-  return result;
 };
 
 const tweetRepliedUsers = async (postId, start_time, end_time) => {
-  const jsTweets = await readOnlyClient.v2.search(`conversation_id:${postId}`, {
-    "tweet.fields": ["conversation_id", "created_at"],
-    expansions: ["author_id"],
-    "user.fields": ["location", "name"],
-    max_results: 100,
-    start_time,
-    end_time,
-  });
+  try {
+    const jsTweets = await readOnlyClient.v2.search(
+      `conversation_id:${postId}`,
+      {
+        "tweet.fields": ["conversation_id", "created_at"],
+        expansions: ["author_id"],
+        "user.fields": ["location", "name"],
+        max_results: 100,
+        start_time,
+        end_time,
+      }
+    );
 
-  const includes = new TwitterV2IncludesHelper(jsTweets);
+    const includes = new TwitterV2IncludesHelper(jsTweets);
 
-  const result = [];
-  for await (const tweet of jsTweets) {
-    const author = includes.author(tweet);
-    // console.log("result", author, tweet);
-    result.push({
-      username: author.username,
-      createdAt: tweet.created_at,
-      postId: tweet.id,
-      postLink: `https://twitter.com/${author.username}/status/${tweet.id}`,
-      postContent: tweet.text,
-    });
+    const result = [];
+    for await (const tweet of jsTweets) {
+      const author = includes.author(tweet);
+      // console.log("result", author, tweet);
+      result.push({
+        username: author.username,
+        createdAt: tweet.created_at,
+        postId: tweet.id,
+        postLink: `https://twitter.com/${author.username}/status/${tweet.id}`,
+        postContent: tweet.text,
+      });
+    }
+    // console.log("result", result);
+    return result;
+  } catch (error) {
+    logger.info(`error due to rate limiting - ${error.data.title}`);
+    if (error.data.status === 429) {
+      logger.info("waiting for rating limiting time");
+      await timer(15 * 60 * 1000);
+      return tweetRepliedUsers(postId, start_time, end_time);
+    }
   }
-  // console.log("result", result);
-  return result;
 };
 
 const getTwitterActionFunc = (action) => {
@@ -141,7 +182,8 @@ const sanitizeTweet = (tweet) => tweet.replace(/\s\[https\:.*]/gm, "");
 // Don't miss #ShibaDoge!`;
 
 // searchTweets(x);
-// tweetLikedUsers();
+// tweetLikedUsers("1624067874276126721");
+// console.log(x);
 // retweetedUsers("1626089401788022784");
 // tweetRepliedUsers("1626089401788022784");
 // followingUsers("1452687837808107520");
