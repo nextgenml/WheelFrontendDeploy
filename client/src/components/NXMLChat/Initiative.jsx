46ade91cb9538a9ccff32d34b2e6ed4efca5d108
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-03-03 01:13:37 +0530

    restructure

---
+++ client/src/components/NXMLChat/Initiative.jsx
@@ -0,0 +1,203 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useParams } from "react-router-dom";
+import { useAccount } from "wagmi";
+import config from "../../config";
+
+const Initiative = ({ prompt, index }) => {
+  const { initiative } = useParams();
+  const { address } = useAccount();
+  const [isChecked, setIsChecked] = useState(false);
+  const [isValidatedFlag, setIsValidatedFlag] = useState(null);
+  const [isPaidFlag, setPaidFlag] = useState(null);
+  const [result, setResult] = useState("");
+  const [link, setLink] = useState("");
+  const [isCopyDisable, setIsCopyDisable] = useState(true);
+  const [isSubmit, setIsSubmit] = useState(true);
+
+  // Toast alert
+  const notify = (msg, toastType) => {
+    if (toastType === "success") {
+      toast.success(msg);
+    } else if (toastType === "info") {
+      toast.info(msg);
+    } else {
+      toast.error(msg);
+    }
+  };
+
+  async function get_gpt_data(input) {
+    const url = "https://backend.chatbot.nexgenml.com/collections";
+    let response = await fetch(url, {
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ msg: input }),
+      method: "POST",
+    });
+    if (response.ok) {
+      let res = await response.json();
+      setResult(
+        res.result +
+          `\nJoin the revolution with NexGen ML\nWebsite: nexgenml.io\nTwitter: https://twitter.com/nextgen_ml\nTelgram: https://t.me/+JMGorMX41tM2NGIx`
+      );
+      setIsCopyDisable(false);
+    } else {
+      notify("Something went wrong. Please try again later", "error");
+    }
+  }
+
+  // link validation
+  const isValidUrl = (urlString) => {
+    let url;
+    try {
+      url = new URL(urlString);
+    } catch (_) {
+      return false;
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
+  };
+
+  useEffect(() => {
+    if (isChecked) {
+      setResult("populating blog");
+      const complete_prompt = `Write a ${Math.floor(
+        Math.random() * (600 - 400 + 1) + 400
+      )} word blog about ${prompt}. Blend in links within the blog  Website: nexgenml.io , Twitter: https://twitter.com/nextgen_ml , Telgram: https://t.me/+JMGorMX41tM2NGIx . Also advise how NexGen ML is playing a crucial role based on the prompt.`;
+      get_gpt_data(complete_prompt);
+    }
+  }, [isChecked]);
+
+  // Check value empty
+  useEffect(() => {
+    if (isChecked && !isCopyDisable && link && isValidUrl(link)) {
+      setIsSubmit(false);
+    } else {
+      setIsSubmit(true);
+    }
+  }, [isCopyDisable, link]);
+
+  async function onSubmitClick() {
+    // send post request to save data in database
+    if (isChecked && !isCopyDisable && link && isValidUrl(link)) {
+      const url = `${config.API_ENDPOINT}/save-blog-data`;
+      let response = await fetch(url, {
+        headers: {
+          accept: "*/*",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          wallet_address: address,
+          initiative,
+          prompt,
+          blog: result,
+          link,
+          validated_flag: isValidatedFlag ? true : null,
+          paid_amount: config.PAID_AMOUNT,
+          paid_flag: isPaidFlag ? true : null,
+        }),
+        method: "POST",
+      });
+
+      let res = await response.text();
+      let data = JSON.parse(res);
+      if (response.ok) {
+        notify(data.msg, "success");
+      } else {
+        notify(data.msg, "danger");
+      }
+    } else {
+      notify("Please provide valid data", "danger");
+    }
+  }
+
+  return (
+    <div className="col-md-6 offset-md-3 col-lg-6 offset-lg-3">
+      <form style={{ color: "white" }}>
+        <div className="row g-3 m-3">
+          <div className="col-sm-12">
+            <span>{prompt}</span>
+          </div>
+          <div className="col-sm-12">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              onClick={() => setIsChecked(!isChecked)}
+            />
+          </div>
+          <div className="col-sm-12">
+            <textarea
+              className="form-control"
+              placeholder="Blog Content"
+              readOnly={true}
+              value={result}
+              style={{ height: "100px" }}
+            ></textarea>
+          </div>
+          {address === config.ADMIN_WALLET_1 && (
+            <>
+              <div className="col-sm-12">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  onClick={() => setIsValidatedFlag(!isValidatedFlag)}
+                />
+                <label>Validate Flag</label>
+              </div>
+              <div className="col-sm-12">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  onClick={() => setPaidFlag(!isPaidFlag)}
+                />
+                <label>Paid Flag</label>
+              </div>
+            </>
+          )}
+          <div className="col-sm-12">
+            <button
+              type="button"
+              className="btn btn-success"
+              disabled={isCopyDisable}
+              onClick={() => {
+                navigator.clipboard.writeText(result);
+                notify("Copied", "info");
+              }}
+            >
+              Copy
+            </button>
+          </div>
+          <div className="col-sm-12">
+            <input
+              type="url"
+              className="form-control"
+              id="link"
+              placeholder="https://www.google.com"
+              value={link}
+              onChange={(e) => setLink(e.target.value)}
+            />
+          </div>
+          <div className="col-sm-12">
+            <button
+              type="button"
+              disabled={isSubmit}
+              className="btn btn-primary"
+              onClick={onSubmitClick}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+        <ToastContainer />
+      </form>
+    </div>
+  );
+};
+export default Initiative;
--- client/src/components/NXMLChat/NXMLChat.jsx
+++ client/src/components/NXMLChat/NXMLChat.jsx
@@ -1,224 +1,32 @@
-import React, { useState, useEffect } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useParams } from "react-router-dom";
 import { useAccount } from "wagmi";
 import config from "../../config";
 import ReactPaginate from "react-paginate";
-
-const Initiative = ({ prompt, index }) => {
-  const { initiative } = useParams();
-  const { address } = useAccount();
-  const [isChecked, setIsChecked] = useState(false);
-  const [isvalidatedFlag, setIsvalidatedFlag] = useState(null);
-  const [ispaidFlag, setpaidFlag] = useState(null);
-  const [result, setResult] = useState("");
-  const [link, setLink] = useState("");
-  const [iscopyDisable, setiscopyDisable] = useState(true);
-  const [isSubmit, setisSubmit] = useState(true);
-
-  // Toast alert
-  const notify = (msg, toastType) => {
-    if (toastType == "success") {
-      toast.success(msg);
-    } else if (toastType === "info") {
-      toast.info(msg);
-    } else {
-      toast.error(msg);
-    }
-  };
-
-  async function get_gpt_data(input) {
-    const url = "https://backend.chatbot.nexgenml.com/collections";
-    let response = await fetch(url, {
-      headers: {
-        accept: "*/*",
-        "accept-language": "en-US,en;q=0.9",
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ msg: input }),
-      method: "POST",
-    });
-    if (response.ok) {
-      let res = await response.json();
-      setResult(
-        res.result +
-          `\nJoin the revolution with NexGen ML\nWebsite: nexgenml.io\nTwitter: https://twitter.com/nextgen_ml\nTelgram: https://t.me/+JMGorMX41tM2NGIx`
-      );
-      setiscopyDisable(false);
-    } else {
-      notify("Something went wrong. Please try again later", "error");
-    }
-  }
-
-  // link validation
-  const isValidUrl = (urlString) => {
-    let url;
-    try {
-      url = new URL(urlString);
-    } catch (_) {
-      return false;
-    }
-    return url.protocol === "http:" || url.protocol === "https:";
-  };
-
-  useEffect(() => {
-    if (isChecked) {
-      setResult("populating blog");
-      const complete_prompt = `Write a ${Math.floor(
-        Math.random() * (600 - 400 + 1) + 400
-      )} word blog about ${prompt}. Blend in links within the blog  Website: nexgenml.io , Twitter: https://twitter.com/nextgen_ml , Telgram: https://t.me/+JMGorMX41tM2NGIx . Also advise how NexGen ML is playing a crucial role based on the prompt.`;
-      get_gpt_data(complete_prompt);
-    }
-  }, [isChecked]);
-
-  // Check value empty
-  useEffect(() => {
-    if (isChecked && !iscopyDisable && link && isValidUrl(link)) {
-      setisSubmit(false);
-    } else {
-      setisSubmit(true);
-    }
-  }, [iscopyDisable, link]);
-
-  async function onSubmitClick() {
-    // send post request to save data in database
-    if (isChecked && !iscopyDisable && link && isValidUrl(link)) {
-      const url = `${config.API_ENDPOINT}/save-blog-data`;
-      let response = await fetch(url, {
-        headers: {
-          accept: "*/*",
-          "accept-language": "en-US,en;q=0.9",
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          wallet_address: address,
-          initiative,
-          prompt,
-          blog: result,
-          link,
-          validated_flag: isvalidatedFlag ? true : null,
-          paid_amount: config.PAID_AMOUNT,
-          paid_flag: ispaidFlag ? true : null,
-        }),
-        method: "POST",
-      });
-
-      let res = await response.text();
-      let data = JSON.parse(res);
-      if (response.status === "200") {
-        notify(data.msg, "success");
-      } else {
-        notify(data.msg, "danger");
-      }
-    } else {
-      notify("Please provide valid data", "danger");
-    }
-  }
-
-  return (
-    <div className="col-md-6 offset-md-3 col-lg-6 offset-lg-3">
-      <form style={{ color: "white" }}>
-        <div className="row g-3 m-3">
-          <div className="col-sm-12">
-            <span>{prompt}</span>
-          </div>
-          <div className="col-sm-12">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              onClick={() => setIsChecked(!isChecked)}
-            />
-          </div>
-          <div className="col-sm-12">
-            <textarea
-              className="form-control"
-              placeholder="Blog Content"
-              readOnly={true}
-              value={result}
-              style={{ height: "100px" }}
-            ></textarea>
-          </div>
-          {address === config.ADMIN_WALLET_1 && (
-            <>
-              <div className="col-sm-12">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  onClick={() => setIsvalidatedFlag(!isvalidatedFlag)}
-                />
-                <label>Validate Flag</label>
-              </div>
-              <div className="col-sm-12">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  onClick={() => setpaidFlag(!ispaidFlag)}
-                />
-                <label>Paid Flag</label>
-              </div>
-            </>
-          )}
-          <div className="col-sm-12">
-            <button
-              type="button"
-              className="btn btn-success"
-              disabled={iscopyDisable}
-              onClick={() => {
-                navigator.clipboard.writeText(result);
-                notify("Copied", "info");
-              }}
-            >
-              Copy
-            </button>
-          </div>
-          <div className="col-sm-12">
-            <input
-              type="url"
-              className="form-control"
-              id="link"
-              placeholder="https://www.google.com"
-              value={link}
-              onChange={(e) => setLink(e.target.value)}
-            />
-          </div>
-          <div className="col-sm-12">
-            <button
-              type="button"
-              disabled={isSubmit}
-              className="btn btn-primary"
-              onClick={onSubmitClick}
-            >
-              Submit
-            </button>
-          </div>
-        </div>
-        <ToastContainer />
-      </form>
-    </div>
-  );
-};
+import Initiative from "./Initiative";
 
 const BlogForm = () => {
   const { address } = useAccount();
   const [prompts, setPrompts] = useState([]);
   const [userData, setUserData] = useState([]);
   const [WalletAdd, setWalletAdd] = useState();
-  const [offset, setoffset] = useState(0);
-  const [pageCount, setpageCount] = useState(0);
+  const [offset, setOffset] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
   const pageSize = 10;
   const [pageNo, setPageNo] = useState(0);
-  const [totalCount, settotalCount] = useState(0);
-  const [isSubmit, setisSubmit] = useState(true);
+  const [totalCount, setTotalCount] = useState(0);
+  const [isSubmit, setIsSubmit] = useState(true);
   let reset = 0;
 
   // Toast alert
   const notify = (msg, toastType) => {
-    if (toastType == "success") {
+    if (toastType === "success") {
       toast.success(msg);
-    } else if (toastType == "info") {
+    } else if (toastType === "info") {
       toast.info(msg);
     } else {
       toast.error(msg);
@@ -256,7 +64,7 @@ const BlogForm = () => {
 
   async function get_user_data(offset) {
     const url = `${config.API_ENDPOINT}/get-blog-data?searchWalletAdd=${
-      reset || WalletAdd == undefined ? "" : WalletAdd
+      reset || WalletAdd === undefined ? "" : WalletAdd
     }&offset=${offset}`;
     let response = await fetch(url, {
       headers: {
@@ -270,8 +78,8 @@ const BlogForm = () => {
     if (response.ok) {
       const result = await response.json();
       setUserData(result?.data ? result.data : 0);
-      settotalCount(result?.totalResult ? result.totalResult : 0);
-      setpageCount(
+      setTotalCount(result?.totalResult ? result.totalResult : 0);
+      setPageCount(
         (result.totalResult ? result.totalResult : 0) < pageSize
           ? 1
           : Math.ceil(result.totalResult / pageSize)
@@ -281,7 +89,7 @@ const BlogForm = () => {
     }
   }
 
-  async function updateflagData(transactionID, vf, pf) {
+  async function updateFlagData(transactionID, vf, pf) {
     let data = {
       transactionID: transactionID,
       validatedFlag: vf,
@@ -300,7 +108,7 @@ const BlogForm = () => {
 
     let result = await response.json();
     // console.log(result);
-    if (response.status == 200) {
+    if (response.status === 200) {
       notify(result.msg, "success");
     } else {
       notify(result.msg, "danger");
@@ -308,10 +116,10 @@ const BlogForm = () => {
   }
 
   async function updateData(flagType, user, index) {
-    if (flagType == "vf") {
-      updateflagData(user.transactionID, !user.validated_flag, user.paid_flag);
+    if (flagType === "vf") {
+      updateFlagData(user.transactionID, !user.validated_flag, user.paid_flag);
     } else {
-      updateflagData(user.transactionID, user.validated_flag, !user.paid_flag);
+      updateFlagData(user.transactionID, user.validated_flag, !user.paid_flag);
     }
     get_user_data(offset);
   }
@@ -319,7 +127,7 @@ const BlogForm = () => {
   const handlePageClick = (event) => {
     setPageNo(event.selected);
     const newOffset = (event.selected * 10) % totalCount;
-    setoffset(newOffset);
+    setOffset(newOffset);
     get_user_data(newOffset);
   };
 
@@ -342,7 +150,7 @@ const BlogForm = () => {
 
   useEffect(() => {
     if (WalletAdd) {
-      setisSubmit(false);
+      setIsSubmit(false);
     }
   }, [WalletAdd]);
 
@@ -357,7 +165,7 @@ const BlogForm = () => {
 
   return (
     <div>
-      {prompts == undefined ? (
+      {prompts === undefined ? (
         <div className="d-flex justify-content-center">Loading...</div>
       ) : prompts.length === 0 ? (
         <div className="d-flex justify-content-center">
@@ -416,7 +224,7 @@ const BlogForm = () => {
                 />
               </div>
             </form>
-            {userData == 0 ? (
+            {userData === 0 ? (
               <div className="d-flex justify-content-center">
                 {/* <div className="spinner-border" style={{ width: "3rem", height: "3rem" }} role="status">
       </div> */}
