984449b344d713d00eec98e6adf4f0edc87ba9b0
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-03-05 18:05:41 +0530

    Update blogs.js

--- repository/blogs.js
+++ repository/blogs.js
@@ -8,7 +8,7 @@ const getBlogStats = async (blogId) => {
 
 const getCustomBlogs = async (walletId, isAdmin, search, pageSize, offset) => {
   const query =
-    "select * from saved_prompts where (1 = ? or wallet_address = ?) and (1 = ? or wallet_address = ?) and initiative = ? order by id desc limit ? offset ?";
+    "select * from saved_prompts where (1 = ? or wallet_address = ?) and (1 = ? or wallet_address = ?) and initiative = ? order by create_date desc limit ? offset ?";
 
   const data = await runQueryAsync(query, [
     isAdmin ? 1 : 0,
