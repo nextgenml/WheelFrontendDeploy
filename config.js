84e2fc5d8c806f0ad67788eff57583941a926033
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-03-09 20:59:51 +0530

    Revert "removed secretes from remote"
    This reverts commit 7713527246779c52beda69907dc3d18069bf95e8.

--- config.js
+++ config.js
@@ -1,39 +1,39 @@
 const prodConfig = {
   // https://randomkeygen.com/
   // this key is used for authentication in end points
-  SECRET_KEY: "",
+  SECRET_KEY: "%S93A-JB@g1Bg12P#SnA.=cmL)vH&O",
   // USDT ADDRESS
-  REWARD_TOKEN: "",
+  REWARD_TOKEN: "0x4bB4954FC47ce04B62F3493040ff8318E4A72981",
   // REFLECTION TOKEN ADDRESS
-  CONTRACT_ADDRESS: "",
+  CONTRACT_ADDRESS: "0x60F2CE0a06E1974a1378322B948567673f6eBF89",
   // TEST NET
   WEB3_PROVIDER_URL:
-    "",
+    "https://eth-goerli.g.alchemy.com/v2/surwT5Ql_QhEc083ru_C98XrwbDj-jVx",
   // REWARD WALLET ADMIN KEY - USDT
   DEPLOYER_WALLET:
-    "",
-  RPC_URL: "",
-  DISABLE_LOGS: false, // bool
-  DB_HOST: "",
+    "f27d03d6b006c9f6fac2cd86e0cad1d61cbb62442056a98f895d81e48cc7c9b1",
+  RPC_URL: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+  DISABLE_LOGS: true,
+  DB_HOST: "prod-mysql",
   TWITTER_DEV_TOKEN: "",
   // user for authenticating admin user
-  ADMIN_WALLET: "",
+  ADMIN_WALLET: "0x49733c8bCd1830ED5300c8d1d0E8432ddE5ed601",
 };
 
 const testConfig = {
-  SECRET_KEY: "",
-  REWARD_TOKEN: "",
-  CONTRACT_ADDRESS: "",
+  SECRET_KEY: "%S93A-JB@g1Bg12P#SnA.=cmL)vH&",
+  REWARD_TOKEN: "0x4bB4954FC47ce04B62F3493040ff8318E4A72981",
+  CONTRACT_ADDRESS: "0x06C28f861Fc4b31b5D2C8CAD880468aEec266F31", // TO DO: CHANGE WALLET ADDRESS HERE
   WEB3_PROVIDER_URL:
-    "",
+    "https://eth-goerli.g.alchemy.com/v2/surwT5Ql_QhEc083ru_C98XrwbDj-jVx",
   DEPLOYER_WALLET:
-    "",
-  RPC_URL: "",
+    "f27d03d6b006c9f6fac2cd86e0cad1d61cbb62442056a98f895d81e48cc7c9b1",
+  RPC_URL: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
   DISABLE_LOGS: false,
-  DB_HOST: "",
+  DB_HOST: "127.0.0.1",
   TWITTER_DEV_TOKEN:
-    "",
-  ADMIN_WALLET: "",
+    "AAAAAAAAAAAAAAAAAAAAAA1ukwEAAAAAlSl4X%2FTjbuuLXmNS8kJNO8MgGz4%3D9uhtUccA0oQNe9rkz9wPhxZ0472SeJhZ5p19YIIXyWtGnoJ6yV",
+  ADMIN_WALLET: "0x49733c8bCd1830ED5300c8d1d0E8432ddE5ed601",
 
 };
 
