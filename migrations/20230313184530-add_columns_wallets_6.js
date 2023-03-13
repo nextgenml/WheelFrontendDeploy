c4f49f3e044b28b3ea1da96bc268f9e9fda0f2fe
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-03-14 00:17:47 +0530

    Create 20230313184530-add_columns_wallets_6.js

---
+++ migrations/20230313184530-add_columns_wallets_6.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(
+      `ALTER TABLE wallets ADD INDEX (wallet_id);`
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
