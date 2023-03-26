cea94309b64ca21617928a01e1ffe3a6f6ef76a2
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-03-26 23:47:56 +0530

    Update referrals.js

--- manager/jobs/referrals.js
+++ manager/jobs/referrals.js
@@ -17,6 +17,6 @@ const initiateProcess = async () => {
   }
 };
 // initiateProcess();
-schedule.scheduleJob("0 */3 * * *", async () => {
+schedule.scheduleJob("0 */1 * * *", async () => {
   await initiateProcess();
 });
