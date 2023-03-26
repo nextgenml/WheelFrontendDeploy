4a5090ccfc1160f9461969ce30281a9d5876b7c3
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-03-27 00:20:46 +0530

    Update blogs.js

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -200,7 +200,7 @@ const saveBlogData = async (req, res) => {
     });
   } catch (error) {
     return res.status(500).json({
-      msg: error.messsage || "Server Error",
+      msg: error.message || "Server Error",
     });
   }
 };
