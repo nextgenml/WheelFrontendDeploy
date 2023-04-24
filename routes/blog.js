af9899e0e5392e6daf6265731fa7df65706fe2ed
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-04-25 01:09:20 +0530

    all are done

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const blogsController = require("../controllers/blogs");
+const { validateLoginSession } = require("./auth");
 
 router.get("/home-page-stats", blogsController.homePageStats);
-router.get("/posted", blogsController.postedBlogs);
-router.put("/posted", blogsController.updatePostedBlogs);
+router.get("/posted", validateLoginSession, blogsController.postedBlogs);
+router.put("/posted", validateLoginSession, blogsController.updatePostedBlogs);
 module.exports = router;
--- routes/referrals.js
+++ routes/referrals.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const referralsController = require("../controllers/referrals");
-const { validateLoginSession } = require("./auth");
+const { validateLoginSession, extractWallet } = require("./auth");
 
 router.post("/", validateLoginSession, referralsController.create);
 router.put("/:id", validateLoginSession, referralsController.update);
 router.get("/", validateLoginSession, referralsController.get);
-router.get("/top", referralsController.topReferrals);
+router.get("/top", extractWallet, referralsController.topReferrals);
 module.exports = router;
