52035fb86f0b867fee8e225d2de57db80c2a7654
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-04-29 17:06:43 +0530

    frontend completed

--- client/src/components/ConverseWithAI/ConverseWithAI.jsx
+++ client/src/components/ConverseWithAI/ConverseWithAI.jsx
@@ -1,18 +1,11 @@
 import { Box, Grid, Typography } from "@mui/material";
-import React from "react";
-import config from "../../config.js";
 import { useAccount } from "wagmi";
-
-let borderStyle = {
-  borderRight: "2px solid #3B7AAA",
-  borderLeft: "2px solid #3B7AAA",
-  borderTop: "2px solid rgb(251, 156, 3)",
-  borderBottom: "2px solid rgb(251, 156, 3)",
-};
+import AIForm from "./AIForm.jsx";
+import styles from "./ConverseWithAI.module.css";
 
 export default function ConverseWithAI() {
-  const {isConnected} = useAccount()
-  if(!isConnected) return null
+  const { isConnected } = useAccount();
+  if (!isConnected) return null;
   return (
     <Box my={5} id="converse_with_ai">
       <Typography
@@ -27,25 +20,8 @@ export default function ConverseWithAI() {
       </Typography>
 
       <Grid container mt={4} spacing={3}>
-        <Grid item xs={12}>
-          <iframe
-            title="converse-with-ai"
-            src="https://chatbot.nexgenml.com/"
-            height="660px"
-            width="100%"
-            style={{
-              ...{
-                border: "0",
-                margin: "0 auto",
-                marginBottom: ".5rem",
-                display: "block",
-                borderRadius: "10px",
-                maxWidth: "960px",
-                minWidth: "300px",
-              },
-              ...borderStyle,
-            }}
-          />
+        <Grid item xs={12} className={styles.aiBox}>
+          <AIForm />
         </Grid>
       </Grid>
     </Box>
