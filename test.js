204b05262f289e121c8e2252a020820f4240ac0d
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-09-21 00:19:52 +0530

    done

---
+++ test.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const ExifParser = require("exif-parser");
+
+// Function to extract metadata from an image file
+function extractImageMetadata(filePath) {
+  const data = fs.readFileSync(filePath);
+  const parser = ExifParser.create(data);
+
+  const result = parser.parse();
+
+  console.log("result", result);
+  const creationDate =
+    result.tags["DateTimeOriginal"] || result.tags["CreateDate"];
+  const downloadDate = new Date(); // You can use the current date as the download date
+
+  return {
+    creationDate: creationDate ? new Date(creationDate * 1000) : null,
+    downloadDate,
+  };
+}
+
+// Usage example
+const imagePath = "s1.jpeg";
+const metadata = extractImageMetadata(imagePath);
+
+console.log("Creation Date:", metadata.creationDate);
+console.log("Download Date:", metadata.downloadDate);
+
+// const fs = require("fs");
+
+// // get creation time using fs.stat() method
+// fs.stat("1694949229700.jpeg", (error, stats) => {
+//   // in case of any error
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+
+//   // else show creation time from stats object
+//   console.log("File created at: ", stats); // File created at:  2021-04-30T23:53:07.633Z
+// });
