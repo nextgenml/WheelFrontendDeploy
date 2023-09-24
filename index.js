d40df075df9c7106d84ad6bd10876da9a8c80677
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-09-24 21:29:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ const { validateLoginSession, extractWalletSoft } = require("./routes/auth");
 const app = express();
 
 app.use(express.json(), express.urlencoded({ extended: true }), cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 app.get("/spinner-data", extractWalletSoft, spinWheelController.spinnerData);
 
