9cee62380081d8af0fa601d1cda0242fdd711414
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-09-27 16:50:32 +0530

    Update LoginHandler.jsx

--- client/src/components/LoginHandler/LoginHandler.jsx
+++ client/src/components/LoginHandler/LoginHandler.jsx
@@ -38,10 +38,13 @@ const LoginHandler = () => {
       alert(`result: ${result}`);
       if (result.address) {
         const data = await loginHolderAPI(result);
-        alert(`data: ${data}`);
+        alert(`data: ${JSON.stringify(data)}}`);
         if (data) {
           setAuthToken(data.token);
+          alert(`signature key in local storage: ${getAuthToken()}`);
           setLoggedInAddress(result.address);
+          alert(`address key in local storage: ${getLoggedInAddress()}`);
+
           window.location.reload();
         }
       }
