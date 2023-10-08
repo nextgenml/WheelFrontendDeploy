bd524cd4e9a407dca34c3197902b1ffd649a7088
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-10-09 00:14:02 +0530

    Update Chores.jsx

--- client/src/components/Twitter/Chores/Chores.jsx
+++ client/src/components/Twitter/Chores/Chores.jsx
@@ -10,8 +10,11 @@ import {
 import { useEffect, useState } from "react";
 import ChoresLevel from "./ChoresLevel";
 import styles from "./Chores.module.css";
+import Loading from "../../loading";
+import { LoadingButton } from "@mui/lab";
 const Chores = () => {
   const [selectedCampaign, setSelectedCampaign] = useState(null);
+  const [computing, setComputing] = useState(false);
   const levels = [
     { level: 1, completed: 20, assigned: 25 },
     { level: 2, completed: 19, assigned: 300 },
@@ -20,6 +23,10 @@ const Chores = () => {
   useEffect(() => {
     fetchData();
   }, [selectedCampaign]);
+
+  const onCompute = () => {
+    setComputing(true);
+  };
   return (
     <Box className={styles.mainBox} textAlign={"center"}>
       <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
@@ -35,9 +42,15 @@ const Chores = () => {
             <MenuItem value={30}>Thirty</MenuItem>
           </Select>
         </FormControl>
-        <Button variant="outlined" sx={{ ml: 2 }}>
+        <LoadingButton
+          variant="outlined"
+          sx={{ ml: 2 }}
+          onClick={onCompute}
+          disabled={computing}
+          loading={computing}
+        >
           Compute my tasks
-        </Button>
+        </LoadingButton>
       </Box>
 
       <List sx={{ mt: 2 }}>
