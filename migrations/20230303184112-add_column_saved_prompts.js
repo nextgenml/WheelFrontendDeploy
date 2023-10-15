393084ee43f347c158ef65a065a7d6ed9056db0d
prabhupuredla <35324834+PrabhuPuredla95@users.noreply.github.com>
2023-10-15 19:14:02 +0530

    Update 20230303184112-add_column_saved_prompts.js

--- migrations/20230303184112-add_column_saved_prompts.js
+++ migrations/20230303184112-add_column_saved_prompts.js
@@ -3,6 +3,41 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("saved_prompts", {
+      id: {
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      transactionID: {
+        type: Sequelize.STRING(255),
+      },
+      wallet_address: {
+        type: Sequelize.STRING(255),
+      },
+      initiative: {
+        type: Sequelize.STRING(255),
+      },
+      prompt: {
+        type: Sequelize.STRING(255),
+      },
+      blog: {
+        type: Sequelize.TEXT,
+      },
+      create_date: {
+        type: Sequelize.DATE,
+      },
+      validated_flag: {
+        type: Sequelize.BOOLEAN,
+      },
+      paid_amount: {
+        type: Sequelize.DOUBLE,
+      },
+      paid_flag: {
+        type: Sequelize.BOOLEAN,
+      },
+    });
     await queryInterface.addColumn(
       "saved_prompts",
       "promoted",
